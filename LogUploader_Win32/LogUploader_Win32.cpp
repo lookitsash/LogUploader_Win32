78b7cc34574e5f0cdbc4481a1d934ca28944ba2d
lookitsash <lookitsash@gmail.com>
2015-03-29 01:05:08 -0400

    cleaned up UI and added threading

--- LogUploader_Win32/LogUploader_Win32.cpp
+++ LogUploader_Win32/LogUploader_Win32.cpp
@@ -1,5 +1,9 @@
 // LogUploader_Win32.cpp : Defines the entry point for the application.
 //
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' " \
+"version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
 
 #include "stdafx.h"
 #include "LogUploader_Win32.h"
@@ -47,14 +51,97 @@ HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
+BOOL FolderCompressionInProgress;
+HWND hwndPB;
+HWND hwndPBLabel;
+
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
-void AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const char *relativePath);
+void ProcessZipAction(HWND hwndParent, HINSTANCE hInstance);
+mz_bool ZipFolder(const char* folderToZip, const char *targetZIPFile);
+mz_bool AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const char *relativePath);
 void listdir (const char *path);
+DWORD WINAPI ThreadRoutine(LPVOID lpArg);
+
+void ProcessZipAction(HWND hwndParent, HINSTANCE hInstance)
+{
+	FolderCompressionInProgress = TRUE;
+
+	RECT client_rectangle;
+	GetClientRect(hwndParent, &client_rectangle);
+	int width = client_rectangle.right - client_rectangle.left;
+	int height = client_rectangle.bottom - client_rectangle.top;
+
+	//HWND hwndPB;    // Handle of progress bar.
+	
+	hwndPB = CreateWindowEx(WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,    
+                                   PROGRESS_CLASS, NULL,   
+                                   WS_CHILD | WS_VISIBLE|PBS_MARQUEE,   
+                                   (width-200)/2, ((height-10)/2)+10, 200,
+                                   20, hwndParent, NULL,   
+                                   hInstance, NULL);
+
+	//SendMessage(hwndPB, PBM_SETRANGE, 0, (LPARAM) MAKELPARAM(0, 599));
+	//SendMessage(hwndPB, PBM_SETPOS, 0, 0);
+	SendMessage(hwndPB, PBM_SETMARQUEE, 1, 0);
+
+	hwndPBLabel = CreateWindow(
+                        TEXT("STATIC"),                   /*The name of the static control's class*/
+                        TEXT("Preparing files for upload..."),                  /*Label's Text*/
+                        WS_CHILD | WS_VISIBLE | SS_LEFT,  /*Styles (continued)*/
+                        (width-180)/2,                                /*X co-ordinates*/
+                        (height/2)-20,                                /*Y co-ordinates*/
+                        180,                               /*Width*/
+                        25,                               /*Height*/
+                        hwndParent,                             /*Parent HWND*/
+                        NULL,              /*The Label's ID*/
+                        hInstance,                        /*The HINSTANCE of your program*/ 
+                        NULL);                            /*Parameters for main window*/
+	
+	
+	//SetBkColor
+
+	DWORD ThreadId;
+	CreateThread(NULL,0,ThreadRoutine,NULL,0,&ThreadId);
+	
+	
+	//DestroyWindow(hwndPB);
+}
+
+DWORD WINAPI ThreadRoutine(LPVOID lpArg)
+{
+    WCHAR userFolder[MAX_PATH];
+	if (SUCCEED(SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, userFolder))) { // Get user home folder
+		char *folderToCompress = (char *)malloc( 1024 );
+		wcstombs(folderToCompress, userFolder, 1024);
+		strcat(folderToCompress, "\\.printnode");
+
+		struct stat sb;
+		if (stat(folderToCompress, &sb) == 0 && S_ISDIR(sb.st_mode)) // Check if target folder exists to zip up
+		{
+			char *targetZIPFile = (char *)malloc( 1024 );
+			wcstombs(targetZIPFile, userFolder, 1024);
+			strcat(targetZIPFile, "\\.printnodeArchive.zip");
+			
+			//mz_bool zipSuccess = ZipFolder(folderToCompress, targetZIPFile); // Zip up target folder
+			mz_bool zipSuccess = ZipFolder("E:\\Pictures\\test\\Subcategories", targetZIPFile); // Zip up target folder
+
+			free(targetZIPFile);
+		}
+		else
+		{
+			// folder does not exist
+		}
+		free(folderToCompress);
+	}
+	FolderCompressionInProgress = FALSE;
+
+	return NULL;
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -77,6 +164,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	LoadString(hInstance, IDC_LOGUPLOADER_WIN32, szWindowClass, MAX_LOADSTRING);
 	MyRegisterClass(hInstance);
 
+
 	// Perform application initialization:
 	if (!InitInstance (hInstance, nCmdShow))
 	{
@@ -85,67 +173,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LOGUPLOADER_WIN32));
 
-	WCHAR userFolder[MAX_PATH];
-	if (SUCCEED(SHGetFolderPathW(NULL, CSIDL_PROFILE, NULL, 0, userFolder))) {
-		char *folderToCompress = (char *)malloc( 1024 );
-		wcstombs(folderToCompress, userFolder, 1024);
-		strcat(folderToCompress, "\\.printnode");
-
-		struct stat sb;
-		if (stat(folderToCompress, &sb) == 0 && S_ISDIR(sb.st_mode))
-		{
-			char *targetZIPFile = (char *)malloc( 1024 );
-			wcstombs(targetZIPFile, userFolder, 1024);
-			strcat(targetZIPFile, "\\.printnodeArchive.zip");
-			remove(targetZIPFile);
-
-			//BEGIN ZIP
-			mz_zip_archive zip;
-			memset(&zip, 0, sizeof(zip));
-
-			if (!mz_zip_writer_init_file(&zip, targetZIPFile, 65537))
-			{
-			//print_error("Failed creating zip archive \"%s\" (1)!\n", pZip_filename);
-				return FALSE;
-			}
-
-			mz_bool success = MZ_TRUE;
-
-			//char* tmpFile = "C:\\Users\\akhan.COLLAGES\\.printnode\\Pic01.jpg";
-
-			AddFolderToZipArchive(&zip, "E:\\Pictures\\test\\Subcategories","");
-			//success &= mz_zip_writer_add_file(&zip, "test/sfds/Pic01.jpg", "C:\\Users\\akhan.COLLAGES\\.printnode\\Pic01.jpg", 0, 0, MZ_BEST_COMPRESSION);
-			//success &= mz_zip_writer_add_file(&zip, "Pic02.jpg", "C:\\Users\\akhan.COLLAGES\\.printnode\\Pic02.jpg", 0, 0, MZ_BEST_COMPRESSION);
-
-			if (!success)
-			{
-				mz_zip_writer_end(&zip);
-				remove(targetZIPFile);
-				//print_error("Failed creating zip archive \"%s\" (2)!\n", pZip_filename);
-				return FALSE;
-			}
-
-			if (!mz_zip_writer_finalize_archive(&zip))
-			{
-				mz_zip_writer_end(&zip);
-				remove(targetZIPFile);
-				//print_error("Failed creating zip archive \"%s\" (3)!\n", pZip_filename);
-				return FALSE;
-			}
-
-			mz_zip_writer_end(&zip);
-			//END ZIP
-
-			free(targetZIPFile);
-		}
-		else
-		{
-			// folder does not exist
-		}
-
-		free(folderToCompress);
-	}
-
+	/*
+	
+	*/
 	// Main message loop:
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
@@ -159,6 +189,52 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	return (int) msg.wParam;
 }
 
+mz_bool ZipFolder(const char* folderToZip, const char *targetZIPFile)
+{
+	remove(targetZIPFile);
+
+	mz_zip_archive zip;
+	memset(&zip, 0, sizeof(zip));
+
+	mz_bool success = MZ_TRUE;
+
+	success &= mz_zip_writer_init_file(&zip, targetZIPFile, 65537);
+	if (!success)
+	{
+		//print_error("Failed creating zip archive \"%s\" (1)!\n", pZip_filename);
+		return success;
+	}
+
+	
+
+	success &= AddFolderToZipArchive(&zip, folderToZip,"");
+	if (!success)
+	{
+		mz_zip_writer_end(&zip);
+		remove(targetZIPFile);
+		//print_error("Failed creating zip archive \"%s\" (2)!\n", pZip_filename);
+		return success;
+	}
+
+	success &= mz_zip_writer_finalize_archive(&zip);
+	if (!success)
+	{
+		mz_zip_writer_end(&zip);
+		remove(targetZIPFile);
+		//print_error("Failed creating zip archive \"%s\" (3)!\n", pZip_filename);
+		return success;
+	}
+
+	success &= mz_zip_writer_end(&zip);
+	if (!success)
+	{
+		remove(targetZIPFile);
+		//print_error("Failed creating zip archive \"%s\" (3)!\n", pZip_filename);
+		return success;
+	}
+	
+	return success;
+}
 
 
 //
@@ -195,16 +271,16 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-void AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const char *relativePath)
+mz_bool AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const char *relativePath)
 {
 	DIR *pdir = NULL;
 	pdir = opendir (path);
 	struct dirent *pent = NULL;
-	if (pdir == NULL) return;
+	if (pdir == NULL) return MZ_FALSE;
 
 	while (pent = readdir (pdir)) // while there is still something in the directory to list
 	{
-		if (pent == NULL) return;
+		if (pent == NULL) return MZ_FALSE;
 		if (pent->d_type == DT_DIR && strcmp(pent->d_name,".") != 0 && strcmp(pent->d_name,"..") != 0)
 		{
 			char* childPath = (char*)malloc(512);
@@ -219,12 +295,19 @@ void AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const c
 			strcat(childRelativePath,pent->d_name);
 			strcat(childRelativePath,"/");
 
-			mz_zip_writer_add_mem(zipArchive, (const char*)childRelativePath, NULL, 0, MZ_BEST_COMPRESSION);
-			
-			AddFolderToZipArchive(zipArchive,childPath,childRelativePath);
-			
+			mz_bool success = mz_zip_writer_add_mem(zipArchive, (const char*)childRelativePath, NULL, 0, MZ_BEST_COMPRESSION);
+			if (success)
+			{
+				success &= AddFolderToZipArchive(zipArchive,childPath,childRelativePath);
+			}
 			free(childPath);
 			free(childRelativePath);
+
+			if (!success)
+			{
+				closedir (pdir);
+				return MZ_FALSE;
+			}
 		}
 		else if (pent->d_type == DT_REG)
 		{
@@ -239,15 +322,21 @@ void AddFolderToZipArchive(mz_zip_archive* zipArchive, const char *path, const c
 			strcat(childRelativePath, relativePath);
 			strcat(childRelativePath,pent->d_name);
 
-			mz_zip_writer_add_file(zipArchive, (const char*)childRelativePath, (const char*)childPath, 0, 0, MZ_BEST_COMPRESSION);
-			
+			mz_bool success = mz_zip_writer_add_file(zipArchive, (const char*)childRelativePath, (const char*)childPath, 0, 0, MZ_BEST_COMPRESSION);			
 			free(childPath);
 			free(childRelativePath);
+
+			if (!success)
+			{
+				closedir (pdir);
+				return MZ_FALSE;
+			}
 		}
 	}
 
 	// finally, let's close the directory
 	closedir (pdir);
+	return MZ_TRUE;
 }
 
 /*
@@ -307,17 +396,24 @@ int main ()
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
+	
+
    HWND hWnd;
 
    hInst = hInstance; // Store instance handle in our global variable
 
    hWnd = CreateWindow(szWindowClass, L"Log Uploader", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-      CW_USEDEFAULT, 0, 400, 300, NULL, NULL, hInstance, NULL);
+      CW_USEDEFAULT, 0, 400, 200, NULL, NULL, hInstance, NULL);
 
    if (!hWnd)
    {
       return FALSE;
    }
+
+   SetTimer(hWnd, 1, 100, NULL);
+
+
+   ProcessZipAction(hWnd, hInstance);
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
@@ -359,10 +455,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
+	case WM_CTLCOLORSTATIC:
+		SetBkColor((HDC)wParam, RGB(255,255,255));
+		return (LRESULT)GetStockObject(HOLLOW_BRUSH);
+		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO: Add any drawing code here...
 		EndPaint(hWnd, &ps);
+		break;
+	case WM_TIMER:
+		if ( wParam == 1 ) {
+			if (!FolderCompressionInProgress)
+			{
+				DestroyWindow(hwndPB);
+				DestroyWindow(hwndPBLabel);
+			}
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
